8b5cc8b818f1013746824f32d057ef09774c7e62
Daniel Mancilla <dm0275@gmail.com>
2019-11-27 13:58:26 -0600

    Added func and tests to file utils

--- files/files.go
+++ files/files.go
@@ -2,7 +2,9 @@ package files
 
 import (
 	"github.com/dm0275/go-utils/errors"
+	"io"
 	"io/ioutil"
+	"os"
 )
 
 /*
@@ -12,4 +14,15 @@ func ReadFile(filePath string) string {
 	text,err := ioutil.ReadFile(filePath)
 	errors.CheckError(err)
 	return string(text)
+}
+
+func OverwriteFile(filePath string, content string, filePermissions os.FileMode) error {
+	file, err := os.OpenFile(filePath, os.O_RDWR, filePermissions)
+	errors.CheckError(err)
+
+	_, err = io.WriteString(file, content)
+	errors.CheckError(err)
+
+	file.Sync()
+	return err
 }
--- files/files_test.go
+++ files/files_test.go
@@ -1,6 +1,11 @@
 package files
 
-import "testing"
+import (
+	"github.com/dm0275/go-utils/errors"
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 
 func TestReadFile(t *testing.T) {
@@ -18,6 +23,53 @@ func TestReadFile(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ReadFile(tt.args.filePath); got != tt.want {
 				t.Errorf("ReadFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOverwriteFile(t *testing.T) {
+	type args struct {
+		filePath string
+		content  string
+		perms  os.FileMode
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "Validate_No_Errors", args: args{filePath: "./sample.txt", content:"Updated", perms: 0644}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := OverwriteFile(tt.args.filePath, tt.args.content, tt.args.perms); (err != nil) != tt.wantErr {
+				t.Errorf("OverwriteFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestOverwriteFile_FileContents(t *testing.T) {
+	type args struct {
+		filePath string
+		content  string
+		perms  os.FileMode
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+	}{
+		{name: "Validate_FileContents", args: args{filePath: "./sample.txt", content:"Updated", perms: 0644}, want: "Updated"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := OverwriteFile(tt.args.filePath, tt.args.content, tt.args.perms)
+			text,fileErr := ioutil.ReadFile("./sample.txt")
+			errors.CheckError(fileErr)
+			if string(text) != tt.want {
+				t.Errorf("OverwriteFile() error = %v, want %v", err, tt.want)
 			}
 		})
 	}
